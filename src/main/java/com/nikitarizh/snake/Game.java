8bec4746534bd4ed418739701128efe87776499e
nikitarizh <nrigworks@gmail.com>
2021-01-12 09:56:54 +0300

    feat: reduced game speed

--- src/main/java/com/nikitarizh/snake/Game.java
+++ src/main/java/com/nikitarizh/snake/Game.java
@@ -16,8 +16,8 @@ public class Game {
     public static volatile Tile food;
     public static Drawer drawer;
     
-    public static final int DRAWING_FREQ = 60;
-    public static final int MOVING_FREQ = 60;
+    public static final int DRAWING_FREQ = 15;
+    public static final int MOVING_FREQ = 15;
 
     public static final int DRAWING_PRIORITY = 0;
     public static final int MOVING_PRIORITY = 1;
